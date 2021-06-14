eba13bb446d8548ca4cf887d8da63d8ba7808f37
kvoss <k.voss@usask.ca>
2021-06-14 10:14:46 -0600

    #8 adds classifiers and opulent MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,17 +5,41 @@ from distutils.core import setup, Extension
 extra_compile_args = []
 extra_compile_args += ['-std=c++17', '-Wall', '-Wextra']
 
+
+CLASSIFIERS = """\
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Developers
+Intended Audience :: Education
+Intended Audience :: Science/Research
+License :: OSI Approved :: BSD License
+Programming Language :: C++
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Topic :: Software Development :: Libraries
+Topic :: Scientific/Engineering
+Topic :: Scientific/Engineering :: Artificial Intelligence
+Topic :: Scientific/Engineering :: Mathematics
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+Operating System :: Unix
+Operating System :: MacOS
+"""
+
+
 setup(name='cbench',
-      version='0.1.0',
+      version='0.2.0',
       author='K. Voss',
       author_email='k.voss@usask.ca',
       url="https://github.com/kvoss/optobench",
+      download_url="https://github.com/kvoss/optobench/releases",
       license="BSD 3-Clause License",
-      classifiers=[
-          "License :: OSI Approved :: BSD License",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.8",
-      ],
+      classifiers=[cs for cs in CLASSIFIERS.split('\n') if cs],
+      platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+      # include_package_data=True,
       ext_modules=[
           Extension('cbench',
                     ['cbench.cc', 'bss.cc'],

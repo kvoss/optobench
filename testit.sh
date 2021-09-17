f35fe9bfaf49fced325d85e930983885fe8a5aa8
kvoss <k.voss@usask.ca>
2021-09-17 16:09:29 -0600

    fix test to use new module name

--- testit.sh
+++ testit.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo "=> Running sanity tests.."
-python3 -c 'import cbench as cb
+python3 -c 'import optobench as ob
 
 xs = [0.1, 0.2, 0.3, 0.4, 0.51]
-for el in dir(cb):
+for el in dir(ob):
     if el.startswith("_"):
         continue
     print(f"-> {el}: ", end="", flush=True)
-    fn = getattr(cb, el)
+    fn = getattr(ob, el)
     if el == "hartmann_6d":
         ans = fn([0.1, 0.2, 0.3, 0.4, 0.51, 0.63])
     else:
@@ -21,5 +21,5 @@ echo "=> Differences:"
 diff -u sanity-test-golden.log sanity-test-instance.log
 
 # echo "Timing test.."
-# python -m timeit -s 'from cbench import michalewicz; xs = [0.1, 0.2, 0.3, 0.4, 0.51]' 'michalewicz(xs)'
+# python -m timeit -s 'from obench import michalewicz; xs = [0.1, 0.2, 0.3, 0.4, 0.51]' 'michalewicz(xs)'
 echo "=> Done."
